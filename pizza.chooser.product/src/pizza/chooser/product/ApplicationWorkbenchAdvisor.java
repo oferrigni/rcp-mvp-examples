f2d3421886a43ae634adbd07f760864017885139
Oliver Ferrigni <oliver.ferrigni@gmail.com>
2012-02-21 19:33:43 -0600

    Added initial generated product.
    Oliver

---
+++ pizza.chooser.product/src/pizza/chooser/product/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,18 @@
+package pizza.chooser.product;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "pizza.chooser.product.perspective"; //$NON-NLS-1$
+
+    public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+        return new ApplicationWorkbenchWindowAdvisor(configurer);
+    }
+
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+}
---
+++ pizza.chooser.product/src/pizza/chooser/product/Perspective.java
@@ -0,0 +1,10 @@
+package pizza.chooser.product;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class Perspective implements IPerspectiveFactory {
+
+	public void createInitialLayout(IPageLayout layout) {
+	}
+}
