6e4112038d90656476b2b48af330c47378132c7f
Oliver Ferrigni <oliver.ferrigni@gmail.com>
2012-02-21 21:28:13 -0600

    Updated perspective to render view.
    Oliver

--- pizza.chooser.product/src/pizza/chooser/product/Perspective.java
+++ pizza.chooser.product/src/pizza/chooser/product/Perspective.java
@@ -5,6 +5,8 @@ import org.eclipse.ui.IPerspectiveFactory;
 
 public class Perspective implements IPerspectiveFactory {
 
-	public void createInitialLayout(IPageLayout layout) {
+	public void createInitialLayout(IPageLayout pageLayout) {
+		pageLayout.setEditorAreaVisible(false);
+		pageLayout.addView("pizza.chooser.ui.application.view", IPageLayout.TOP, 1f, pageLayout.getEditorArea());
 	}
 }
